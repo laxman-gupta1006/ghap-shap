5940be7de2cfd389ab7ec8239bf05e8eeadb1f73
Laxman <laxman.gupta1006@gmail.com>
2020-12-08 13:16:47 +0530

    initialize firebase

---
+++ src/misc/Firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+var Config = {
+   apiKey: "AIzaSyBSEUtukuFNZ9I3ML02TtMC_YMtUQC9p5M",
+   authDomain: "ghap-shap-9391b.firebaseapp.com",
+   projectId: "ghap-shap-9391b",
+   storageBucket: "ghap-shap-9391b.appspot.com",
+   messagingSenderId: "590628544093",
+   appId: "1:590628544093:web:9f757a8c60dd5e6923f7b4",
+   measurementId: "G-FW6DZ95FMN"
+ };
+ const app=firebase.initializeApp(Config)
